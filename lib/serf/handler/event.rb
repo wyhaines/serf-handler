b69b2c749f23e70398e2eb23b0300e2c338fc8d2
Kirk Haines <kirk-haines@cookpad.com>
2017-10-24 07:35:22 -0600

    Safe naviation on env check.

--- lib/serf/handler/event.rb
+++ lib/serf/handler/event.rb
@@ -4,7 +4,7 @@ module Serf
       attr_accessor :type, :name, :payload
 
       def initialize(cli = {})
-        @type = cli[:type] || ENV['SERF_EVENT'].to_sym || :query
+        @type = cli[:type] || ENV['SERF_EVENT']&.to_sym || :query
         @type = :event if @type == :user
         @name = cli[:name] || ( @type == :query ? ENV['SERF_QUERY_NAME'] : ENV['SERF_USER_EVENT'] )
         @payload = cli[:payload] || STDIN.read
